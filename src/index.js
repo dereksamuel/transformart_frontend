076665b269da7cf2d2aaf60dcd4f9f48840afd99
Derek Paul <dereksamuelgr@gmail.com>
2022-03-15 17:58:51 -0500

    initialReducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { store } from "./utils/store";
 import { App } from "./components/App";
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+  </Provider>,
   document.getElementById("root")
 );
